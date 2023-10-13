d0be029129b5ddbb1737c395be5dd15bf4951419
nirbighnamishra <nirbighna811@gmail.com>
2023-10-13 16:03:56 +0530

    client view added

---
+++ clientView/client-view/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
